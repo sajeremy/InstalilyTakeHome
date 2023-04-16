75be9fa0751de07230f3c56d79ee3554182c37ee
“sajeremy” <9jsantiago4@gmail.com>
2023-04-16 16:38:21 -0400

    initial tests of individual web scrapes

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const xml2js = require("xml2js");
 const path = require("path");
-const { pdpUrls, totalUrlArr } = require("./public/javascripts/urlParse");
+const {
+  pdpUrls,
+  totalUrlArr,
+  scrapeProductPage,
+} = require("./public/javascripts/urlParse");
 
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -26,8 +30,26 @@ app.get("/test", async (req, res) => {
   const jcrewURL = "https://www.jcrew.com/sitemap-wex/sitemap-index.xml";
   const pdpUrlList = await pdpUrls(jcrewURL);
   const jcrewUrlTotal = await totalUrlArr(pdpUrlList);
+  const scrapedInfo = await scrapeProductPage(jcrewUrlTotal[0]);
 
-  return res.json(jcrewUrlTotal);
+  return res.json(scrapedInfo);
+});
+
+app.get("/scrapepage", async (req, res) => {
+  const url1 =
+    "https://www.jcrew.com/p/boys/categories/clothing/active/kids-reebok-club-c-sneakers/BF021";
+  const url2 =
+    "https://www.jcrew.com/p/boys/categories/clothing/graphics-shop/graphic-t-shirts/boys-now-or-never-t-shirt/AV603";
+  const url3 =
+    "https://www.jcrew.com/p/girls/categories/accessories/jewelry/bracelets/girls-glitter-braid-bracelet/64022";
+  const url4 =
+    "https://www.jcrew.com/p/home/categories/bedding-and-bath/accessories/apotheke-charcoal-lotion/M9536";
+  const scrapedInfo1 = await scrapeProductPage(url1);
+  const scrapedInfo2 = await scrapeProductPage(url2);
+  const scrapedInfo3 = await scrapeProductPage(url3);
+  const scrapedInfo4 = await scrapeProductPage(url4);
+
+  return res.json({ scrapedInfo1, scrapedInfo2, scrapedInfo3, scrapedInfo4 });
 });
 
 module.exports = app;
--- public/javascripts/urlParse.js
+++ public/javascripts/urlParse.js
@@ -1,6 +1,7 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const xml2js = require("xml2js");
+const cheerio = require("cheerio");
 
 const pdpUrls = async (url) => {
   const response = await fetch(url);
@@ -29,4 +30,43 @@ const totalUrlArr = async (urlArr) => {
   return totalUrlList;
 };
 
-module.exports = { pdpUrls, totalUrlArr };
+const scrapeProductPage = async (url) => {
+  const res = await fetch(url);
+  const html = await res.text();
+  const $ = cheerio.load(html);
+
+  const productName = $("#product-name__p").text().trim();
+  const productCategory = $(".breadcrumbs a").last().text().trim();
+  const productDescription = $(".product-info .description").text().trim();
+  const productImages = $(".product-detail-hero-image")
+    .map(function () {
+      return $(this).attr("src");
+    })
+    .get();
+  const productPrice = $('.product-info [itemprop="price"]').attr("content");
+  const productAvailability = $('.product-info [itemprop="availability"]').attr(
+    "content"
+  );
+  const productReviews = $(".reviews .review")
+    .map(function () {
+      const rating = $(this).find(".rating-stars").attr("data-rating");
+      const author = $(this).find(".review-author").text().trim();
+      const text = $(this).find(".review-text").text().trim();
+      return { rating, author, text };
+    })
+    .get();
+
+  const infoObject = {
+    productName,
+    productCategory,
+    productDescription,
+    productImages,
+    productPrice,
+    productAvailability,
+    productReviews,
+  };
+
+  return infoObject;
+};
+
+module.exports = { pdpUrls, totalUrlArr, scrapeProductPage };
